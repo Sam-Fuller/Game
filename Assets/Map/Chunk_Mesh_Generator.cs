1749ef12eaae29fd756e5f19a23c7c5cede1c134
Alphasam123 <Alphasam123@gmail.com>
2020-08-23 11:55:53 +0100

    GAME-12 roof and floor perlin noise functions

--- Assets/Map/Chunk_Mesh_Generator.cs
+++ Assets/Map/Chunk_Mesh_Generator.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -9,7 +9,12 @@ using static MeshBuilder;
 [RequireComponent(typeof(MeshFilter))]
 public class Chunk_Mesh_Generator : MonoBehaviour {
 
-    Mesh mesh;
+    public static int levelSize = 200;
+    public static int perlinRoofHeight = 10;
+
+    Mesh mesh; 
+
+    bool[][] map;
 
     // Start is called before the first frame update
     void Start() {
@@ -33,6 +38,73 @@ public class Chunk_Mesh_Generator : MonoBehaviour {
     void Update() {
         
     }
+
+    // int[] generateRoofPerlin(int startNoise, int size){
+    //     double[] PerlinSeed = new double[size];
+	// 	double[] perlin = new double[size];
+
+    //     for (int i = 0; i < size; i++) {
+    //         PerlinSeed[i] = UnityEngine.Random.value;
+    //         perlin[i] = 0;
+    //     }
+
+    //     PerlinSeed[0] = startNoise;
+
+    //     for (int stepSize = 1; stepSize < size; stepSize *= 2) {
+    //         for (int i = 0; i < size; i++) {
+    //             double low = PerlinSeed[stepSize*(int)(i/stepSize)];
+
+    //             int highPos = stepSize*(int)(i/stepSize) + stepSize;
+    //             highPos = highPos > size-1? size-1: highPos; 
+    //             double high = PerlinSeed[highPos];
+
+    //             double noise = (high - low) * (i%stepSize) + low;
+
+    //             perlin[i] += noise;
+    //         }
+    //     }
+
+    //     int[] outPerlin = new int[size];
+    //     for (int i = 0; i < size; i++) {
+    //         outPerlin[i] = (int) perlin[i];
+    //     }
+
+    //     return outPerlin;
+    // }
+
+    // int[] generateFloorPerlin(int startNoise, int size){
+    //     double[] PerlinSeed = new double[size];
+	// 	double[] perlin = new double[size];
+
+    //     for (int i = 0; i < size; i++) {
+    //         PerlinSeed[i] = UnityEngine.Random.value;
+    //         perlin[i] = 0;
+    //     }
+
+    //     PerlinSeed[0] = startNoise;
+
+    //     for (int stepSize = 1; stepSize < size; stepSize *= 2) {
+    //         for (int i = 0; i < size; i++) {
+    //             double low = PerlinSeed[stepSize*(int)(i/stepSize)];
+
+    //             int highPos = stepSize*(int)(i/stepSize) + stepSize;
+    //             highPos = highPos > size-1? size-1: highPos; 
+    //             double high = PerlinSeed[highPos];
+
+    //             double noise = (high - low) * (i%stepSize) + low;
+
+    //             perlin[i] += noise;
+    //         }
+    //     }
+
+    //     int[] outPerlin = new int[size];
+    //     for (int i = 0; i < size; i++) {
+    //         outPerlin[i] = (int) perlin[i];
+    //     }
+
+    //     return outPerlin;
+    // }
+
 
     void generateMap(int prevExit, int prevExitPerlinHeight) {
         map = new bool[levelSize*2][levelSize];
